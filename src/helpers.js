e323a7d0f5302c0649a3bc470509a584d78679a3
kwan-s <stkwan@protonmail.com>
2021-08-26 15:04:28 -0400

    add helper function

--- src/helpers.js
+++ src/helpers.js
@@ -2,3 +2,7 @@ export const getRandomizedNumber = (min, max) => {
   let randomNumber = Math.random() * (max - min) + min;
   return Number.parseInt(randomNumber);
 };
+
+export const removeHtmlMarkupFromString = (description) => {
+  return description.replace(/<\/?[^>]+(>|$)/g, '');
+};
