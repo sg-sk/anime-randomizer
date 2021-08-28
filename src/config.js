71a78331ec77533e0016192fcfb522468d70ac69
sg-sk <sk.gaming.1337@protonmail.com>
2021-08-28 14:30:36 -0400

    update config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
 const API_URL = 'https://graphql.anilist.co/';
-const ANILIST_MAX_PAGE = 79;
+const ANILIST_URL_PREFIX_ANIME = 'https://anilist.co/anime/';
+const ANILIST_MAX_PER_PAGE = 50;
+const ANILIST_MAX_PAGE = 30;
 
-export { API_URL, ANILIST_MAX_PAGE };
+export {
+  API_URL,
+  ANILIST_URL_PREFIX_ANIME,
+  ANILIST_MAX_PER_PAGE,
+  ANILIST_MAX_PAGE,
+};
