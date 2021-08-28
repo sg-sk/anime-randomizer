9412905f683818bc36bf424d5750c9d30b0c4181
sg-sk <sk.gaming.1337@protonmail.com>
2021-08-28 14:27:41 -0400

    add spinner component

---
+++ src/components/Spinner/Spinner.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Spinner = styled.div`
+  order: 5px solid #eee;
+  border-top: 5px solid #eee;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 0.8s linear infinite;
+  margin: 20px auto;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
