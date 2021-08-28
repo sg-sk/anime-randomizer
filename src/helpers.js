7b07028220e6bb61ed7259356debd02d2c38d751
sg-sk <sk.gaming.1337@protonmail.com>
2021-08-28 14:29:02 -0400

    update helper function

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+import { ANILIST_URL_PREFIX_ANIME } from './config';
+
 export const getRandomizedNumber = (min, max) => {
   let randomNumber = Math.random() * (max - min) + min;
   return Number.parseInt(randomNumber);
@@ -5,4 +7,8 @@ export const getRandomizedNumber = (min, max) => {
 
 export const removeHtmlMarkupFromString = (description) => {
   return description.replace(/<\/?[^>]+(>|$)/g, '');
+};
+
+export const concatenateAnilistId = (id) => {
+  return ANILIST_URL_PREFIX_ANIME + id;
 };
