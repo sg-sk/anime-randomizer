0b214fc05a308c9fab8903609a3befd1d9494f80
sg-sk <sk.gaming.1337@protonmail.com>
2021-08-29 19:00:06 -0400

    update config file

--- src/config.js
+++ src/config.js
@@ -2,10 +2,12 @@ const API_URL = 'https://graphql.anilist.co/';
 const ANILIST_URL_PREFIX_ANIME = 'https://anilist.co/anime/';
 const ANILIST_MAX_PER_PAGE = 50;
 const ANILIST_MAX_PAGE = 30;
+const MAX_RECENT_ANIME_RESULTS = 5;
 
 export {
   API_URL,
   ANILIST_URL_PREFIX_ANIME,
   ANILIST_MAX_PER_PAGE,
   ANILIST_MAX_PAGE,
+  MAX_RECENT_ANIME_RESULTS,
 };
