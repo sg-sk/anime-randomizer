08730e1be9d157db3ba21b86e4e0326233dd1315
sg-sk <sk.gaming.1337@protonmail.com>
2021-08-29 19:01:23 -0400

    add functionalities to display the most recent anime results

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import Button from './Button';
+import Grid from './Grid';
 import Spinner from './Spinner';
+import Thumbnail from './Thumbnail';
 import { useAnimeFetch } from '../hooks/useAnimeFetch';
 import { concatenateAnilistId, removeHtmlMarkupFromString } from '../helpers';
 
 const Home = () => {
-  const { randomAnime, loading, error, setIsFetchNewRandomAnime } =
-    useAnimeFetch();
+  const {
+    randomAnime,
+    randomAnimeList,
+    loading,
+    error,
+    setIsFetchNewRandomAnime,
+  } = useAnimeFetch();
 
   if (loading) return <Spinner />;
   if (error) return <div>Something went wrong...</div>;
 
   return (
     <div>
-      <a href={concatenateAnilistId(randomAnime.id)}>View More</a>
-      <br />
       <span>{randomAnime.title.romaji}</span>
       <br />
       <span>{removeHtmlMarkupFromString(randomAnime.description)}</span>
+      <br />
       <Button
         text='Random Anime'
         callback={() => setIsFetchNewRandomAnime(true)}
       />
       <br />
       <img src={randomAnime.coverImage.large} alt='large-cover-img' />
+      <br />
+      <a href={concatenateAnilistId(randomAnime.id)}>Find More On AniList</a>
+      <br />
+      <Grid header='Recent Anime Results'>
+        {randomAnimeList
+          ? randomAnimeList.map((anime) => (
+              <Thumbnail
+                key={anime.id}
+                anilistLink={concatenateAnilistId(anime.id)}
+                image={anime.coverImage.large}
+              />
+            ))
+          : null}
+      </Grid>
     </div>
   );
 };
--- src/hooks/useAnimeFetch.js
+++ src/hooks/useAnimeFetch.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
-import { ANILIST_MAX_PER_PAGE } from '../config';
-import { getRandomizedNumber } from '../helpers';
+import { ANILIST_MAX_PER_PAGE, MAX_RECENT_ANIME_RESULTS } from '../config';
+import { getRandomizedNumber, isPersistedState } from '../helpers';
 import API from '../API';
 
 export const useAnimeFetch = () => {
   const [randomAnime, setRandomAnime] = useState({});
+  const [randomAnimeList, setRandomAnimeList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [isFetchingNewRandomAnime, setIsFetchNewRandomAnime] = useState(false);
@@ -18,7 +19,6 @@ export const useAnimeFetch = () => {
       const animeInfoArray = fullAnimeInfosJson.data.Page.media;
       const randomAnimeInfo =
         animeInfoArray[getRandomizedNumber(0, ANILIST_MAX_PER_PAGE - 1)];
-      console.log(randomAnimeInfo);
 
       setRandomAnime(randomAnimeInfo);
       setLoading(false);
@@ -30,6 +30,13 @@ export const useAnimeFetch = () => {
   // Initial Render
   useEffect(() => {
     fetchAnimeInfos();
+
+    // Display recent anime results from localStorage
+    const localState = isPersistedState('homeState');
+    if (localState) {
+      setRandomAnimeList(localState);
+      return;
+    }
   }, []);
 
   // Fetch new random anime
@@ -37,8 +44,25 @@ export const useAnimeFetch = () => {
     if (!isFetchingNewRandomAnime) return;
 
     fetchAnimeInfos();
+    setRandomAnimeList((previousRandomAnimeInfos) => {
+      // Shift first anime object if it reaches the maximum displayed anime results
+      if (previousRandomAnimeInfos.length === MAX_RECENT_ANIME_RESULTS)
+        previousRandomAnimeInfos.shift();
+      return [...previousRandomAnimeInfos, randomAnime];
+    });
     setIsFetchNewRandomAnime(false);
-  }, [isFetchingNewRandomAnime]);
+  }, [randomAnime, isFetchingNewRandomAnime]);
 
-  return { randomAnime, loading, error, setIsFetchNewRandomAnime };
+  // Write to localStorage
+  useEffect(() => {
+    localStorage.setItem('homeState', JSON.stringify(randomAnimeList));
+  }, [randomAnimeList]);
+
+  return {
+    randomAnime,
+    randomAnimeList,
+    loading,
+    error,
+    setIsFetchNewRandomAnime,
+  };
 };
