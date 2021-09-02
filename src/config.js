fa4ec90dd325f18d3ba1668f685751ef8d24425b
sg-sk <sk.gaming.1337@protonmail.com>
2021-09-01 21:26:28 -0400

    update max recent anime number

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const API_URL = 'https://graphql.anilist.co/';
 const ANILIST_URL_PREFIX_ANIME = 'https://anilist.co/anime/';
 const ANILIST_MAX_PER_PAGE = 50;
 const ANILIST_MAX_PAGE = 30;
-const MAX_RECENT_ANIME_RESULTS = 5;
+const MAX_RECENT_ANIME_RESULTS = 7;
 
 export {
   API_URL,
