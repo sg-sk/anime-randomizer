47e14f937c73b9bc0056b48a24a0af2a23331299
sg-sk <sk.gaming.1337@protonmail.com>
2021-09-22 17:08:41 -0400

    add header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div className='p-2 bg-blue-500 text-white font-extrabold flex justify-between'>
+    <h1 className='py-2 px-4'>Anime Randomizer</h1>
+  </div>
+);
+
+export default Header;
