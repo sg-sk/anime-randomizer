f9abf6a6a222260abb985dd0bf0ac9f86b1190b3
Stephen <sk.gaming.1337@protonmail.com>
2021-10-21 20:44:34 -0400

    redesign HeroPoster component and its styling

--- src/components/HeroPoster/HeroPoster.styles.js
+++ src/components/HeroPoster/HeroPoster.styles.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
+export const Banner = styled.div`
+  background-image: url(${({ image }) => image});
+  background-position: center 35%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 400px;
+`;
+
 export const Information = styled.div`
   @media screen and (max-width: 640px) {
     margin-top: 2rem;
   }
+`;
+
+export const Thumbnail = styled.div`
+  margin-top: -100px;
 `;
--- src/components/HeroPoster/index.js
+++ src/components/HeroPoster/index.js
@@ -1,35 +1,64 @@
 import React from 'react';
+import HeroBackground from '../HeroBackground';
 import HeroThumbnail from '../HeroThumbnail';
 import {
   concatenateAnilistId,
   removeHtmlMarkupFromString,
+  concatenateGenres,
 } from '../../helpers';
-import { Information } from './HeroPoster.styles';
+import { Information, Thumbnail } from './HeroPoster.styles';
 
 const HeroPoster = ({ anime }) => (
-  <div className='sm:flex'>
-    <div className='flex justify-center sm:flex-none'>
+  <>
+    {anime.bannerImage ? (
+      <HeroBackground imageUrl={anime.bannerImage} />
+    ) : (
+      <div className='h-32' />
+    )}
+    <Thumbnail className='flex justify-center mb-5'>
       <HeroThumbnail
         anilistLink={concatenateAnilistId(anime.id)}
         image={anime.coverImage.large}
       />
-    </div>
-    <Information className='sm:mx-8'>
-      <p className='mb-2 font-black'>
-        {anime.title.romaji} ({anime.seasonYear ? anime.seasonYear : <>TBD</>})
-      </p>
-      <div className='sm:inline-flex bg-blue-500 text-white text-center font-extrabold mb-2 px-4 py-5 shadow rounded'>
-        {anime.averageScore ? <>{anime.averageScore}%</> : <>N/A</>}
+    </Thumbnail>
+    <div className='mx-5 md:mx-20 lg:mx-40 xl:mx-60 2xl:mx-80'>
+      <div className='sm:flex'>
+        <Information className='w-full h-full mb-5'>
+          <h1 className='mb-2 font-black text-center text-3xl'>
+            {anime.title.romaji}
+          </h1>
+          <p className='mb-2 italic text-center text-lg'>
+            {concatenateGenres(anime.genres)}
+          </p>
+          <p className='mb-2 text-xl'>
+            {anime.description ? (
+              removeHtmlMarkupFromString(anime.description)
+            ) : (
+              <>No Description</>
+            )}
+          </p>
+          <div className='mb-2 sm:flex sm:gap-2'>
+            <div className='flex-auto p-3 mb-2 sm:mb-0 rounded-lg bg-blue-300 text-lg'>
+              <h4 className='font-semibold'>Score</h4>
+              <p>{anime.averageScore ? anime.averageScore : <>TBA</>}</p>
+            </div>
+            <div className='flex-auto p-3 mb-2 sm:mb-0 rounded-lg bg-blue-300 text-lg'>
+              <h4 className='font-semibold'>Year</h4>
+              <p>{anime.seasonYear ? anime.seasonYear : <>TBA</>}</p>
+            </div>
+            <div className='flex-auto p-3 mb-2 sm:mb-0 rounded-lg bg-blue-300 text-lg'>
+              <h4 className='font-semibold'>Episodes</h4>
+              <p>{anime.episodes ? anime.episodes : <>TBA</>}</p>
+            </div>
+            <div className='flex-auto p-3 mb-2 sm:mb-0 rounded-lg bg-blue-300 text-lg'>
+              <h4 className='font-semibold'>Status</h4>
+              <p>{anime.status ? anime.status : <>TBA</>}</p>
+            </div>
+          </div>
+        </Information>
       </div>
-      <p className='text-justify'>
-        {anime.description ? (
-          removeHtmlMarkupFromString(anime.description)
-        ) : (
-          <>No Description</>
-        )}
-      </p>
-    </Information>
-  </div>
+    </div>
+  </>
 );
 
 export default HeroPoster;
