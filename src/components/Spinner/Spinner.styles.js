077c179d28f83ab453d996c20f1e30f61ab6e8d5
Stephen <sk.gaming.1337@protonmail.com>
2021-10-21 20:43:08 -0400

    update spinner styling

--- src/components/Spinner/Spinner.styles.js
+++ src/components/Spinner/Spinner.styles.js
@@ -4,10 +4,10 @@ export const Spinner = styled.div`
   order: 5px solid #eee;
   border-top: 5px solid #353535;
   border-radius: 50%;
-  width: 50px;
-  height: 50px;
+  width: 100px;
+  height: 100px;
   animation: spin 0.8s linear infinite;
-  margin: 20px auto;
+  margin: 500px auto;
   @keyframes spin {
     0% {
       transform: rotate(0deg);
