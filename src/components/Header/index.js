686f709ea5a8c2c694137337e84a045fe8f0bb57
Stephen <sk.gaming.1337@protonmail.com>
2021-10-21 20:46:22 -0400

    update header styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Header = () => (
-  <div className='p-2 bg-blue-500 text-white font-extrabold flex justify-between'>
-    <h1 className='py-2 px-4'>Anime Randomizer</h1>
+  <div className='p-2 bg-blue-500 text-white font-bold flex justify-between'>
+    <h1 className='py-2 px-4'>AR</h1>
   </div>
 );
 
