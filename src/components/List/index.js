7ce1e0b94ef345f0f294d60ce3746236553cbc31
Stephen <sk.gaming.1337@protonmail.com>
2021-10-21 20:42:49 -0400

    add List component

---
+++ src/components/List/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const List = ({ children }) => (
+  <div className='pb-5 mx-5 md:mx-20 lg:mx-40 xl:mx-60 2xl:mx-80 space-y-4'>
+    {children}
+  </div>
+);
+
+export default List;
