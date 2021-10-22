b2ea6a744b91b293f8bac7ae80e5670eb4f7dae3
Stephen <sk.gaming.1337@protonmail.com>
2021-10-21 20:43:23 -0400

    add Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className='p-2 bg-blue-500 text-white flex justify-center'>
+    <h1 className='py-2 px-4'>
+      Made with 💖 by{' '}
+      <a
+        href='https://github.com/sg-sk'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='underline'
+      >
+        sg-sk
+      </a>
+    </h1>
+  </div>
+);
+
+export default Footer;
