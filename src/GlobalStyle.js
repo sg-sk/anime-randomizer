6332bc26f95e121f3b1703a956b0b021fcf0597c
Stephen <sk.gaming.1337@protonmail.com>
2021-10-21 20:40:04 -0400

    update global styling

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,6 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     * {
+        box-sizing: border-box;
         font-family: 'Overpass', sans-serif;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
     }
 `;
