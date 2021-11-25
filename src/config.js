152b8ca055ef0044eb458df5e9259841b1905555
Stephen <89104245+sg-sk@users.noreply.github.com>
2021-11-24 23:16:15 -0500

    Update ANILIST_MAX_PAGE variable to display more popular animes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const API_URL = 'https://graphql.anilist.co/';
 const ANILIST_URL_PREFIX_ANIME = 'https://anilist.co/anime/';
 const ANILIST_MAX_PER_PAGE = 50;
-const ANILIST_MAX_PAGE = 30;
+const ANILIST_MAX_PAGE = 10;
 const MAX_RECENT_ANIME_RESULTS = 5;
 
 export {
